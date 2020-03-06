c9d8957b0838addb3de58af79b26569a015cf644
patel-malav <patelmalav64@gmail.com>
2020-03-06 22:33:30 +0530

    restructuring

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { GraphQLModule } from './graphql.module';
+import { GraphQLModule } from './graphql/graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
