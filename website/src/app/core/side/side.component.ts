0a540118be50246c659ddff6f807f2139558a698
patel-malav <patelmalav64@gmail.com>
2020-03-11 03:01:38 +0530

    show taxanomy info

--- website/src/app/core/side/side.component.ts
+++ website/src/app/core/side/side.component.ts
@@ -1,32 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { CanvasService } from 'src/app/explore/canvas/canvas.service';
-import { Apollo } from 'apollo-angular';
-import gql from 'graphql-tag';
-import { map } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { CanvasService } from "src/app/explore/canvas/canvas.service";
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'pb-side',
-  templateUrl: './side.component.html',
-  styleUrls: ['./side.component.scss']
+  selector: "pb-side",
+  templateUrl: "./side.component.html",
+  styleUrls: ["./side.component.scss"]
 })
 export class SideComponent implements OnInit {
-
   public term: string;
-  public data: Observable<any>;
+  public case: string = "search";
+  public search$: Observable<any>;
+  public observation$: Observable<any>;
+  public taxanomy$: Observable<any>;
 
-  obs: Observable<any>;
-  constructor(private apollo: Apollo ,private canvasServ: CanvasService) { }
+  constructor(private apollo: Apollo, private canvasServ: CanvasService) {}
 
-  ngOnInit(): void { }
-  
+  ngOnInit(): void {}
+
   search(value: string) {
-    this.data = this.apollo.query<any>({query: gql`{search(term:"${value}"){taxaId name image}}`}).pipe(map(resp => {
-      console.log(resp.data);
-      return resp.data.search
-    }));
-      // this.obs = this.dataServ.getObservation(id);
-      // this.canvasServ.showObservation(id);
+    this.search$ = this.apollo
+      .query<any>({
+        query: gql`{search(term: "${value}"){taxaId name {common sci} image}}`
+      })
+      .pipe(map(resp => resp.data.search));
+      this.case = 'search';
   }
 
+  taxanomy(id: number) {
+    console.log(id);
+    this.taxanomy$ = this.apollo
+      .query<any>({ query: gql`{taxanomy(id:${id}){id name{common sci}image{id medium}}}` })
+      .pipe(map(resp => resp.data.taxanomy));
+    this.case = 'taxanomy';
+    // this.apollo
+    // .query<any>({query: gql`{}`})
+  }
+
+  observation(id: number) {
+    this.observation$ = this.apollo
+      .query<any>({
+        query: gql`{observation(id:${id}){id user{id name}geo{lat long}images{id medium}}}`
+      })
+      .pipe(map(resp => resp.data.observation));
+    // this.canvasServ.showObservation(id);
+  }
 }
